f2c904b32e2b63a30192be9ba5bf6b3b9218d07a
houssame errjem <errjem2@gmail.com>
2023-10-05 16:16:23 +0100

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import "./Home.css";
-import ProgressBar from "react-bootstrap/ProgressBar";
+// import ProgressBar from "react-bootstrap/ProgressBar";
 
 function Home() {
-  const now = 15;
   return (
     <>
       <div className="home">
         <div className="second-home">
-          <h1>
+          <section className="container">
+            <div className="list">
+              <div className="item">
+                <span className="item-txt">HI, ITS YAHYAVISION</span>
+                <span className="item-d">
+                  <p className="item-dot dot"></p>
+                </span>
+              </div>
+            </div>
+            <div className="list">
+              <div className="item">
+                <span className="item-txt">HI, ITS YAHYAVISION</span>
+                <span className="item-d">
+                  <p className="item-dot dot"></p>
+                </span>
+              </div>
+            </div>
+          </section>
+          {/* <h1>
             This Website is <br /> Under Construction !
           </h1>
           <br />
@@ -17,9 +34,9 @@ function Home() {
             striped
             animated
             variant="danger"
-            now={now}
-            label={`${now}%`}
-          />
+            now={15}
+            label={`${15}%`}
+          /> */}
         </div>
       </div>
     </>
