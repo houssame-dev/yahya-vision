71483df3ba33165a342c0439eb57198536d54488
houssame errjem <errjem2@gmail.com>
2023-10-05 22:38:53 +0100

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./Home.css";
+import { Col, Row } from "react-bootstrap";
+import {
+  FaSquareBehance,
+  FaSquareInstagram,
+  FaSquareXTwitter,
+  FaLinkedin,
+} from "react-icons/fa6";
 
 function Home() {
   return (
@@ -22,6 +29,42 @@ function Home() {
               </div>
             </div>
           </section>
+          <Row className="row">
+            <Col md={3} className="text-start my-auto py-3 px-3">
+              <h1>Graphic Designer</h1>
+            </Col>
+            <Col md={6} className="text-start my-auto py-3 px-3">
+              <h5>
+                As a <span>graphic designer</span>, I'm here to turn your vision
+                into compelling visuals. From custom logos to striking marketing
+                materials, I specialize in delivering top-notch design solutions
+                that make your brand shine. Let's collaborate and bring your
+                creative ideas to life!
+              </h5>
+            </Col>
+            <Col md={3} className="social-media">
+              <div>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <FaSquareBehance />
+                </a>
+              </div>
+              <div>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <FaSquareInstagram />
+                </a>
+              </div>
+              <div>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <FaSquareXTwitter />
+                </a>
+              </div>
+              <div>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin />
+                </a>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     </>
